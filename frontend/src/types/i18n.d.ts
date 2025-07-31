9fd0d1188671a3ba0c3f85ae26e8e8f8f53e036d
Masykuri Syaifullah <msyaifullah@gmail.com>
2025-07-31 10:58:50 +0700

    define ptype safety object

--- frontend/src/types/i18n.d.ts
+++ frontend/src/types/i18n.d.ts
@@ -1,19 +1,8 @@
+interface TranslationObject {
+  [key: string]: string | TranslationObject;
+}
+
 declare module '*.json' {
-  const value: any;
-  export default value;
-}
-
-declare module '../../locales/en.json' {
-  const value: any;
-  export default value;
-}
-
-declare module '../../locales/id.json' {
-  const value: any;
-  export default value;
-}
-
-declare module '../../locales/cn.json' {
-  const value: any;
+  const value: TranslationObject;
   export default value;
 } 
